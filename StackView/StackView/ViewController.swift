b9a4aecb26fc9d2514432d445726d57b2fd01cf2
SeongKookKIM <ksk8646@naver.com>
2024-05-20 13:02:14 +0900

    UISwich 사용하기

--- StackView/StackView/ViewController.swift
+++ StackView/StackView/ViewController.swift
@@ -8,37 +8,65 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let toggleSwitch = UISwitch()
+    let label = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        /*
+         let stackView = UIStackView()
+         stackView.axis = .vertical
+         stackView.spacing = 10
+         stackView.alignment = .center
+         stackView.distribution = .fillEqually // 배치
+         
+         let topLabel = UILabel()
+         topLabel.text = "위"
+         topLabel.textAlignment = .center
+         topLabel.backgroundColor = .lightGray
+         
+         let leftLabel = UILabel()
+         leftLabel.text = "왼쪽"
+         leftLabel.textAlignment = .center
+         leftLabel.backgroundColor = .lightGray
+         
+         let rigthLabel = UILabel()
+         rigthLabel.text = "오른쪽"
+         rigthLabel.textAlignment = .center
+         rigthLabel.backgroundColor = .lightGray
+         
+         let hStackView = UIStackView(arrangedSubviews: [leftLabel, rigthLabel])
+         hStackView.axis = .horizontal
+         hStackView.spacing = 10
+         
+         stackView.addArrangedSubview(topLabel)
+         stackView.addArrangedSubview(hStackView)
+         
+         view.addSubview(stackView)
+         
+         stackView.translatesAutoresizingMaskIntoConstraints = false
+         
+         NSLayoutConstraint.activate([
+         stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+         stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+         stackView.widthAnchor.constraint(equalToConstant: 200),
+         stackView.heightAnchor.constraint(equalToConstant: 100)
+         ])
+         */
         
-        let stackView = UIStackView()
+        /*
+         
+         */
+     
+        label.text = "Switch is OFF"
+        label.textAlignment = .center
+        
+        toggleSwitch.addTarget(self, action: #selector(switchChanged), for: .valueChanged)
+        
+        let stackView = UIStackView(arrangedSubviews: [label, toggleSwitch])
         stackView.axis = .vertical
         stackView.spacing = 10
         stackView.alignment = .center
-        stackView.distribution = .fillEqually // 배치
-        
-        let topLabel = UILabel()
-        topLabel.text = "위"
-        topLabel.textAlignment = .center
-        topLabel.backgroundColor = .lightGray
-        
-        let leftLabel = UILabel()
-        leftLabel.text = "왼쪽"
-        leftLabel.textAlignment = .center
-        leftLabel.backgroundColor = .lightGray
-        
-        let rigthLabel = UILabel()
-        rigthLabel.text = "오른쪽"
-        rigthLabel.textAlignment = .center
-        rigthLabel.backgroundColor = .lightGray
-        
-        let hStackView = UIStackView(arrangedSubviews: [leftLabel, rigthLabel])
-        hStackView.axis = .horizontal
-        hStackView.spacing = 10
-        
-        stackView.addArrangedSubview(topLabel)
-        stackView.addArrangedSubview(hStackView)
         
         view.addSubview(stackView)
         
@@ -47,12 +75,15 @@ class ViewController: UIViewController {
         NSLayoutConstraint.activate([
             stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            stackView.widthAnchor.constraint(equalToConstant: 200),
-            stackView.heightAnchor.constraint(equalToConstant: 100)
         ])
-        
     }
-
-
+    
+    @objc func switchChanged(_ sender: UISwitch) {
+        if sender.isOn {
+            label.text = "Switch is ON"
+        } else {
+            label.text = "Switch is OFF"
+        }
+    }
 }
 
