6c965f02672caa83c75c3a22c202c73c7ebb47b6
SeongKookKIM <ksk8646@naver.com>
2024-05-20 15:26:06 +0900

    Text Styling

--- Text/Text/ViewController.swift
+++ Text/Text/ViewController.swift
@@ -11,7 +11,38 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let label = UILabel()
+        label.text = "Stylized Text"
+        /*
+         label.textColor = .red
+         label.backgroundColor = .yellow
+         label.textAlignment = .left
+         label.font = UIFont.systemFont(ofSize: 17)
+         label.numberOfLines = 0
+         label.lineBreakMode = .byWordWrapping
+         */
+
+        
+        label.font = UIFont.boldSystemFont(ofSize: 17)
+        label.textColor = .red
+        label.backgroundColor = .yellow
+        label.textAlignment = .center
+        
+        label.layer.cornerRadius = 5
+        label.clipsToBounds = true
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(label)
+        
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.topAnchor.constraint(equalTo: view.topAnchor, constant: 150),
+            label.widthAnchor.constraint(equalToConstant: 200),
+            label.heightAnchor.constraint(equalToConstant: 40)
+            
+        ])
     }
 
 
