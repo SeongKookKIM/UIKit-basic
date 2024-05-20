c4f11eccf62fae3c39de96c2b14ffb4631e360c0
SeongKookKIM <ksk8646@naver.com>
2024-05-20 17:44:06 +0900

    Image Shadow

--- Image/Image/ViewController.swift
+++ Image/Image/ViewController.swift
@@ -17,9 +17,23 @@ class ViewController: UIViewController {
          */
         
         let imageView = UIImageView(image: UIImage(systemName: "hare.fill"))
-        imageView.frame = CGRect(x: 50, y: 50, width: 100, height: 50)
+        imageView.frame = CGRect(x: 50, y: 50, width: 100, height: 100)
         imageView.tintColor = .purple
         imageView.contentMode = .scaleAspectFit
+        
+        imageView.layer.cornerRadius = imageView.frame.width / 2
+        imageView.clipsToBounds = true
+        
+        imageView.layer.shadowColor = UIColor.red.cgColor
+        imageView.layer.shadowOpacity = 0.8
+        imageView.layer.shadowOffset = CGSize(width: 10, height: 10)
+        imageView.layer.shadowRadius = 6
+        
+        imageView.layer.borderColor = UIColor.blue.cgColor
+        imageView.layer.borderWidth = 10
+        
+        imageView.alpha = 0.75
+        
         self.view.addSubview(imageView)
         
         let flagImageView = UIImageView(image: UIImage(systemName: "flag"))
