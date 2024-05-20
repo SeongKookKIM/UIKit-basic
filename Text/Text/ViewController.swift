a0ecb1e6c1da9e432205ee57ba10206b309db98b
SeongKookKIM <ksk8646@naver.com>
2024-05-20 16:10:05 +0900

    NSMutableAttributedString text styling

--- Text/Text/ViewController.swift
+++ Text/Text/ViewController.swift
@@ -17,6 +17,23 @@ class ViewController: UIViewController {
          label.text = "Stylized Text"
          */
         
+        let attributedString = NSMutableAttributedString(string: "Hello, World")
+        attributedString.addAttribute(.font, value: UIFont.italicSystemFont(ofSize: 20), range: NSRange(location: 0, length: 5))
+        attributedString.addAttribute(.foregroundColor, value: UIColor.blue, range: NSRange(location: 7, length: 5))
+        attributedString.addAttribute(.underlineStyle, value: NSUnderlineStyle.single.rawValue, range: NSRange(location: 0, length: 12))
+        
+        let label = UILabel()
+        label.attributedText = attributedString
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(label)
+        
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.topAnchor.constraint(equalTo: view.topAnchor, constant: 200)
+        ])
+        
         /*
          label.textColor = .red
          label.backgroundColor = .yellow
