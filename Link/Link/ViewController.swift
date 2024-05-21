452099f2512fff681e43db05a30b1805bd9e5dcc
SeongKookKIM <ksk8646@naver.com>
2024-05-21 15:08:39 +0900

    Initialize Link Project

---
+++ Link/Link/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Link
+//
+//  Created by mac on 5/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
