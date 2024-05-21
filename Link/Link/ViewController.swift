0b932d7569bb9f2f34d0e0726e18f628e02da998
SeongKookKIM <ksk8646@naver.com>
2024-05-21 15:29:45 +0900

    Add WebView

--- Link/Link/ViewController.swift
+++ Link/Link/ViewController.swift
@@ -6,8 +6,17 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
+    
+    private lazy var webKit: WKWebView = {
+        let webkit = WKWebView(frame: .zero)
+        webkit.translatesAutoresizingMaskIntoConstraints = false
+ 
+        
+        return webkit
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,22 +28,36 @@ class ViewController: UIViewController {
         linkButton.titleLabel?.font = UIFont.systemFont(ofSize: 18)
         linkButton.configuration = config
         
-        linkButton.addAction(UIAction { _ in
+        linkButton.addAction(UIAction { [weak self] _ in
             guard let url = URL(string: "https://www.apple.com") else {
                 return
             }
-            UIApplication.shared.open(url)
+            self?.openInWebView(url: url)
         }, for: .touchUpInside)
         
         linkButton.translatesAutoresizingMaskIntoConstraints = false
         self.view.addSubview(linkButton)
         
+        self.view.addSubview(webKit)
+        webKit.isHidden = true
+        
         NSLayoutConstraint.activate([
             linkButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-            linkButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+            linkButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            
+            webKit.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            webKit.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            webKit.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            webKit.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor)
         ])
+        
     }
 
+    func openInWebView(url: URL) {
+        let request = URLRequest(url: url)
+        webKit.load(request)
+        webKit.isHidden = false
+    }
 
 }
 
