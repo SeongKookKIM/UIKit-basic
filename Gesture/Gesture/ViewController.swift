a3519b34eab226df6b3b5152600bfe18ec083c20
SeongKookKIM <ksk8646@naver.com>
2024-05-22 09:44:59 +0900

    PinchGesture

--- Gesture/Gesture/ViewController.swift
+++ Gesture/Gesture/ViewController.swift
@@ -12,22 +12,31 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let rectangle = UIView()
-        rectangle.backgroundColor = .yellow
-        rectangle.frame = CGRect(x: 100, y: 100, width: 175, height: 125)
-        rectangle.isUserInteractionEnabled = true
+        /* Gesture
+         let rectangle = UIView()
+         rectangle.backgroundColor = .yellow
+         rectangle.frame = CGRect(x: 100, y: 100, width: 175, height: 125)
+         rectangle.isUserInteractionEnabled = true
+         
+         self.view.addSubview(rectangle)
+         
+         // let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+         // let gesture = UILongPressGestureRecognizer(target: self, action: #selector(handleGesture))
+         
+         // gesture.numberOfTapsRequired = 2 // 탭을 두 번해야함
+         // gesture.minimumPressDuration = 2.0
+         
+         rectangle.addGestureRecognizer(gesture)
+         */
         
-        self.view.addSubview(rectangle)
+        let imageView = UIImageView(image: UIImage(systemName: "star.fill"))
+        imageView.frame = CGRect(x: 120, y: 300, width: 200, height: 200)
+        imageView.isUserInteractionEnabled = true
         
-        // let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap))
-        let gesture = UILongPressGestureRecognizer(target: self, action: #selector(handleGesture))
+        self.view.addSubview(imageView)
         
-        // gesture.numberOfTapsRequired = 2 // 탭을 두 번해야함
-        gesture.minimumPressDuration = 2.0
-        
-        rectangle.addGestureRecognizer(gesture)
-        
-        
+        let gesture = UIPinchGestureRecognizer(target: self, action: #selector(hanlderImageGesture))
+        imageView.addGestureRecognizer(gesture)
     }
     /* UITapGestureRecognizer
      @objc func handleGesture(_ sender: UITapGestureRecognizer) {
@@ -36,13 +45,23 @@ class ViewController: UIViewController {
          }
      }
      */
+    
+    /* UILongPressGestureRecognizer
+     @objc func handleGesture(_ sender: UILongPressGestureRecognizer) {
+         print(sender.state)
+         if let view = sender.view, sender.state == .began {
+             view.backgroundColor = (view.backgroundColor == .yellow) ? .red : .yellow
+         }
+     }
+     */
 
-    @objc func handleGesture(_ sender: UILongPressGestureRecognizer) {
-        print(sender.state)
-        if let view = sender.view, sender.state == .began {
-            view.backgroundColor = (view.backgroundColor == .yellow) ? .red : .yellow
+    @objc func hanlderImageGesture(_ sender: UIPinchGestureRecognizer) {
+        print(sender.scale)
+        if let view = sender.view {
+            view.transform = view.transform.scaledBy(x: sender.scale, y: sender.scale)
         }
     }
+
 
 }
 
