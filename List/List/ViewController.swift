4305af7a21bb76cf5bf81bc92c74bb7a0295c55d
SeongKookKIM <ksk8646@naver.com>
2024-05-22 14:43:19 +0900

    Group TableList

--- List/List/ViewController.swift
+++ List/List/ViewController.swift
@@ -7,32 +7,80 @@
 
 import UIKit
 
+struct Animal {
+    let name: String
+}
+
+struct AnimalCategory {
+    let category: String
+    let animals: [Animal]
+}
+
 class ViewController: UIViewController, UITableViewDataSource {
 
-    let items = ["고양이", "강아지", "새", "파충류", "물고기"]
+    // let items = ["고양이", "강아지", "새", "파충류", "물고기"]
+    let categories = [
+        AnimalCategory(category: "포유류", animals: [Animal(name: "고양이"), Animal(name: "강아지")]),
+        AnimalCategory(category: "파충류", animals: [Animal(name: "거북이"), Animal(name: "도마뱀")]),
+        
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let tableView = UITableView(frame: self.view.bounds)
+        let tableView = UITableView(frame: self.view.bounds, style: .grouped)
         tableView.dataSource = self
+        
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         self.view.addSubview(tableView)
         
-    }
+        /* SImple Table
+         let tableView = UITableView(frame: self.view.bounds)
+         tableView.dataSource = self
+         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+         self.view.addSubview(tableView)
+         */
 
+        
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return categories.count
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
+        return categories[section].animals.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return categories[section].category
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell") ?? UITableViewCell(style: .default, reuseIdentifier: "cell")
         var config = cell.defaultContentConfiguration()
-        config.text = items[indexPath.row]
+        config.text = categories[indexPath.row].animals[indexPath.row].name
         cell.contentConfiguration = config
         
         return cell
     }
+    
+    /* Simple Table
+     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+         return items.count
+     }
+     
+     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+         let cell = tableView.dequeueReusableCell(withIdentifier: "cell") ?? UITableViewCell(style: .default, reuseIdentifier: "cell")
+         var config = cell.defaultContentConfiguration()
+         config.text = items[indexPath.row]
+         cell.contentConfiguration = config
+         
+         return cell
+     }
+     */
+
+
 
 }
 
