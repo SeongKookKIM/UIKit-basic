d86b2ef9a48a35c1b4f37570157c91ea29f4603c
SeongKookKIM <ksk8646@naver.com>
2024-05-22 13:39:05 +0900

    윤년추가

--- CustomDatePicker/CustomDatePicker/ViewController.swift
+++ CustomDatePicker/CustomDatePicker/ViewController.swift
@@ -8,9 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
-
+    
     private lazy var datePicker: UIPickerView = {
-       let picker = UIPickerView()
+        let picker = UIPickerView()
         picker.delegate = self
         picker.dataSource = self
         
@@ -20,7 +20,11 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     let yaers = [Int](2000...2150)
     let months = [Int](1...12)
     let days = [Int](1...31)
-
+    
+    var selectedYear = 0
+    var selectedMonth = 0
+    var selectedDay = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -32,7 +36,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
             datePicker.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
         ])
     }
-
+    
     // MARK: - UIPickerViewDataSource
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         3
@@ -45,12 +49,19 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         case 1:
             return months.count
         case 2:
-            return days.count
+            if selectedMonth == 2 {
+                return (((selectedYear % 4) == 0) &&
+                        ((selectedYear % 100) != 0) || selectedYear % 400 == 0) ? 29 : 28
+            } else if [4,6,9,11].contains(selectedMonth) {
+                return 30
+            } else {
+                return days.count
+            }
         default:
             return 0
         }
     }
-
+    
     // MARK: - UIPicekrViewDelegate
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         switch component {
@@ -62,6 +73,21 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
             return String(describing: days[row])
         default:
             return nil
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        switch component {
+        case 0:
+            selectedYear = yaers[row]
+            pickerView.reloadComponent(2)
+        case 1:
+            selectedMonth = months[row]
+            pickerView.reloadComponent(2)
+        case 2:
+            selectedDay = days[row]
+        default:
+            return
         }
     }
 }
