7b446682d6563a239130c3b85cd099f8be15433e
SeongKookKIM <ksk8646@naver.com>
2024-05-23 15:38:53 +0900

    GroupBoxView 생성

---
+++ Form/Form/GroupBoxViewController.swift
@@ -0,0 +1,20 @@
+//
+//  GroupBoxViewController.swift
+//  Form
+//
+//  Created by mac on 5/23/24.
+//
+
+import UIKit
+
+class GroupBoxViewController: UIViewController {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
