3eb11e8af3f283b2ee7626284dc15dcf589bb136
SeongKookKIM <ksk8646@naver.com>
2024-05-23 16:46:12 +0900

    페이지 전환

--- Navigation/Navigation/ViewController.swift
+++ Navigation/Navigation/ViewController.swift
@@ -11,9 +11,49 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.navigationItem.title = "네비게이션 타이틀"
+        // self.title = "네비게이션 타이틀" 둘 다 가능
+        
+        // 스토리보드에서 임베디드 안했을 경우 nil
+        self.navigationController?.navigationBar.barStyle = .default
+        self.navigationController?.navigationBar.isTranslucent = true
+        self.navigationController?.navigationBar.backgroundColor = .systemBrown
+        self.navigationController?.navigationBar.scrollEdgeAppearance?.backgroundColor = .systemBrown
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white.cgColor]
+        
+        let leftButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(leftButtonTapped))
+        self.navigationItem.leftBarButtonItem = leftButton
+        
+        let rightButton = UIBarButtonItem(title: "완료", style: .done, target: self, action: #selector(rightButtonTapped))
+
+        let extraButton = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(extraButtonTapped))
+        
+        self.navigationItem.rightBarButtonItems = [rightButton, extraButton]
+        
+        let button = UIButton(type: .system)
+        button.setTitle("다음 화면으로", for: .normal)
+        button.addAction(UIAction { [weak self] _ in
+            let nextViewController = UIViewController()
+            nextViewController.view.backgroundColor = .red
+            self?.show(nextViewController, sender: nil)
+        }, for: .touchUpInside)
+        
+        button.frame = CGRect(x: 100, y: 100, width: 200, height: 50)
+        
+        view.addSubview(button)
     }
 
+    @objc func leftButtonTapped() {
+        print("왼쪽 버튼 탭")
+    }
+    
+    @objc func rightButtonTapped() {
+        print("오른쪽 버튼 탭")
+    }
+    
+    @objc func extraButtonTapped() {
+        print("추가 버튼 탭")
+    }
 
 }
 
